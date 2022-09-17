31a73dd63a30bb3bd878e4c4b7b29b9c59edfd03
Yuka <yukasakamaki.goldware@gmail.com>
2022-09-17 10:31:20 +0900

    draft devise setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,36 @@
 Rails.application.routes.draw do
-    # ヘルスチェック
-    get '/health_check', to: 'health_checks#index'
-    # 記事一覧
-    get '/blogs', to: 'blogs#index'
-    # 記事詳細  
-    get '/blogs/:id', to: 'blogs#show'
-    # 記事作成
-    post '/blogs', to: 'blogs#create'
+  post "/graphql", to: "graphql#execute"
+  # ヘルスチェック
+  get '/health_check', to: 'health_checks#index'
+  # 記事一覧
+  get '/blogs', to: 'blogs#index'
+  # 記事詳細  
+  get '/blogs/:id', to: 'blogs#show'
+  # 記事作成
+  post '/blogs', to: 'blogs#create'
+  # mount_devise_token_auth_for 'User', at: 'auth',
+  # controllers: {
+  #     registrations: 'auth/registrations'
+  #   }
+  #   # ログインユーザー取得のルーティング
+  #   namespace :auth do
+  #     resources :sessions, only: %i[index]
+  #   end
+  namespace :api do
+    namespace :v1 do
+      resources :test, only: %i[index]
+
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
+    end
+  end
+
+    if Rails.env.development?
+      mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+    end
 end
