a950ee637873b98dbc050cd9df5c5f5a099912f3
Yuka <yukasakamaki.goldware@gmail.com>
2022-10-08 22:14:33 +0900

    change domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000', "https://web.golfdorone.tech"
+      origins 'http://localhost:3000', "https://web.golf-dorone.com"
   
       resource '*',
         headers: :any,
